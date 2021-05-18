d00a4a1a5c329956ba086cddf13abefe9ef7bd52
Olesia <odobrogor@gmail.com>
2021-05-18 19:12:11 +0300

    asyncAfter with alertController

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -30,8 +30,10 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         
         fetchImage()
-
-        // Do any additional setup after loading the view.
+        
+        delay(3) {
+            self.loginAlert()
+        }
     }
     
     fileprivate func fetchImage(){
@@ -40,7 +42,38 @@ class SecondViewController: UIViewController {
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
         
-        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
-        self.image = UIImage(data: imageData)
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        
+        DispatchQueue.main.async {
+            self.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    fileprivate func loginAlert(){
+        let ac = UIAlertController(title: "Зарегистрированы?", message: "Введите логин и пароль", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+        
+        ac.addTextField { (usernameTF) in
+            usernameTF.placeholder = "Введите логин"
+        }
+        ac.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "Введите пароль"
+            userPasswordTF.isSecureTextEntry = true
+        }
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()){
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
     }
 }
